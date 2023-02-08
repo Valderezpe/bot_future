1a58692f6fb791df33fbc34c631681cf2e62aa41
valderez <valderezpe@gmail.com>
2023-02-08 20:28:20 -0300

    projecto bot finalizado.

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const WebSocket = require("ws");
 const axios = require("axios");
 const crypto = require("crypto");
 
-const ws = new WebSocket(process.env.STREAM_URL + "btcusdt@bookTicker");
+const ws = new WebSocket(process.env.STREAM_URL + "btcusdt@markPrice@1s");
 
 let isOpened = false;
 
 ws.onmessage = async (event) => {
   const obj = JSON.parse(event.data);
   console.log("Symbol:" + obj.s);
-  console.log("Price:" + obj.a);
+  console.log("Mark Price:" + obj.p);
 
-  const price = parseFloat(obj.a);
-  if (price < 23169 && !isOpened) {
-    console.log("pode Comprar!!");
-    newOrder("BTCUSDT", "0.001", "BUY");
-    isOpened = true;
-  } else if (price > 23171 && !isOpened) {
-    console.log("Hora de Vender");
+  const price = parseFloat(obj.p);
+  if (price < 22970 && !isOpened) {
+    console.log("vender!!");
     newOrder("BTCUSDT", "0.001", "SELL");
+    isOpened = true;
+  } else if (price <= 22940 && isOpened) {
+    console.log("Comprar");
+    newOrder("BTCUSDT", "0.001", "BUY");
     isOpened = false;
   }
 };
@@ -38,7 +38,7 @@ async function newOrder(symbol, quantity, side) {
 
   const result = await axios({
     method: "POST",
-    url: process.env.API_URL + "/v3/order?" + new URLSearchParams(data),
+    url: process.env.API_URL + "/v1/order?" + new URLSearchParams(data),
     headers: { "X-MBX-APIKEY": process.env.API_KEY },
   });
   console.log(result.data);
